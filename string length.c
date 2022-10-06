ab9ebc9a318879a7f5d52bb30e865df62f9c7554
NAGAVARSHINI <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-06 13:32:22 +0530

    Add files via upload

---
+++ string length.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int length(char s[100]);
+int main(void) {
+  int len;
+  char str[100];
+  printf("enter the string");
+  scanf("%s", str);
+  len = length(str);
+  printf("the length of string is %d", len);
+  return 0;
+}
+int length(char s[100])
+{
+  int i, count = 0;
+  for (i = 0; s[i] != '\0'; i++)
+    count++;
+  return count;
+}
