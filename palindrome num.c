d12d7fb96a3e30296b6aba8a1d51d28c72dc5ac8
NAGAVARSHINI <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-07 07:55:11 +0530

    Add files via upload

---
+++ palindrome num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+  int num, temp, rev;
+  printf("Enter the number:\n");
+  scanf("%d", &num);
+  printf("The reversed number is:\n");
+  while (num != 0) {
+    temp = num % 10;
+    printf("%d", temp);
+    num = num / 10;
+  }
+  num = rev;
+  if (rev == num)
+    printf("\npalindrome");
+  else
+    printf("\n not a palindrome");
+
+  return 0;
+}
