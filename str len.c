d6fd30c0c7b6856cb13441a4cb1519c2b18ced06
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-13 00:18:31 +0530

    Add files via upload

---
+++ str len.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  char str1[100];
+  int len = 0;
+  printf("Enter string whose length is to be found: ");
+  scanf("%s", str1);
+  while (str1[len] != '\0')
+    len++; // here the length of string is calculated.
+  printf("Length of the string is %d", len);
+}
