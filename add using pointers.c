6eb1f238057c4cff0e9bd7f1accac56eb2eb0a5c
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-14 21:56:15 +0530

    Add files via upload

---
+++ add using pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<math.h>
+void update(int *a,int *b) {
+      int pa,pb;  
+    pa = *a;
+    pb = *b;  
+    *a = pa + pb;
+    *b = abs(pa - pb);
+}
+
+int main(void) {
+   int a, b;
+  printf("enter the value of a and b\n");
+  scanf("%d%d",&a,&b);
+    int *pa = &a, *pb = &b;
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+  return 0;
+  
+}
