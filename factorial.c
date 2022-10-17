8f5b9bf46bb34c3ffcd6bacf9e9afae23f7cb6a8
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-17 23:26:58 +0530

    factorial of a number

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  long int ajfact(int);
+  int n;
+  printf("Enter the number\n");
+  scanf("%d", &n);
+  printf("\nThe Factorial of %d is %ld", n, ajfact(n));
+  return 0;
+}
+long int ajfact(int n) {
+  if (n <= 1)
+    return 1;
+  else
+    return (n * ajfact(n - 1));
+}
