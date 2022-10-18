760116cf005fa7f48bcb2766b41e61d0cd63f065
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-18 23:39:39 +0530

    binomial coefficient (nCr)

---
+++ binomial coef.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int factorial(int);
+int main(void) {
+  int n, r, binom;
+printf("Enter value of n and r\n");
+scanf("%d%d", &n, &r);
+if(n<0 || r<0 || n<r)
+printf("Invalid Input");
+else
+{
+  binom =factorial(n)/(factorial(r)*factorial(n-r));
+printf("Result = %d",binom);
+
+}
+  return 0;
+}
+int factorial(int n)
+{
+if(n == 0)
+return 1;
+
+else
+
+return(n*factorial(n-1));
+}
