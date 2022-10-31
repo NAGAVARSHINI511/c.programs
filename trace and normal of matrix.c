0af0b62fd86f3f1fc4613f75bad5109d7f29b4e3
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-31 23:38:22 +0530

    Update trace and normal of matrix.c

--- trace and normal of matrix.c
+++ trace and normal of matrix.c
@@ -13,11 +13,11 @@ int main(void) {
       scanf("%d", &A[i][j]);
       sum = sum + A[i][j] * A[i][j];
     }
-  }
-  normal = sqrt(sum);
+  }                            #make sure it is a square matrix
+  normal = sqrt(sum);          #normal fo matrix is sum of squares of each element in an matrix
   printf("The normal of the given matrix is = %.2f\n", normal);
   for (i = 0; i < n; ++i) {
-    trace = trace + A[i][i];
+    trace = trace + A[i][i];   #trace is the sum of diagonal elements of an matrix 
   }
   printf("Trace of the matrix is = %d\n", trace);
   return 0;
