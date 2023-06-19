cdecd6d9534889dc306ffee944af8229b8584c81
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2023-06-19 21:14:18 +0530

    Update bubble sort.c

--- bubble sort.c
+++ bubble sort.c
@@ -10,9 +10,9 @@ void main()
   for(i=0;i<n;i++)
   scanf("%d",&a[i]);
   {
-  for(i=0;i<n-1;i++)
+  for(i=0;i<n-1;i++)  //this for loop indicates pass
    {
-    for(j=0;j<n-1-i;j++)
+    for(j=0;j<n-1-i;j++)  //this for loop is to compare
     if(a[i]>a[i+1])
     {
      temp=a[i];
